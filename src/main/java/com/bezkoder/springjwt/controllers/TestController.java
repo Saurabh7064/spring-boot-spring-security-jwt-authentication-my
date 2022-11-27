4661aa7bbffe85db2a49d76408cdd91f3999ce6c
saurabh <saurabh.kumar7064@gmail.com>
2022-11-27 16:52:00 -0600

    working file upload and saving to db

--- src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+++ src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -1,12 +1,23 @@
 package com.bezkoder.springjwt.controllers;
 
+import com.bezkoder.springjwt.models.Tutorial;
+import com.bezkoder.springjwt.payload.response.ResponseMessage;
+import com.bezkoder.springjwt.repository.ExcelHelper;
+import com.bezkoder.springjwt.repository.ExcelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
@@ -31,5 +42,54 @@ public class TestController {
   @PreAuthorize("hasRole('ADMIN')")
   public String adminAccess() {
     return "Admin Board.";
+  }
+
+  @Autowired
+  ExcelService fileService;
+
+  @PostMapping("/upload")
+  public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+    String message = "";
+
+    if (ExcelHelper.hasExcelFormat(file)) {
+      try {
+        fileService.save(file);
+
+        message = "Uploaded the file successfully: " + file.getOriginalFilename();
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+      } catch (Exception e) {
+        message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+      }
+    }
+
+    message = "Please upload an excel file!";
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+  }
+
+  @GetMapping("/tutorials")
+  public ResponseEntity<List<Tutorial>> getAllTutorials() {
+    try {
+      List<Tutorial> tutorials = fileService.getAllTutorials();
+
+      if (tutorials.isEmpty()) {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+
+      return new ResponseEntity<>(tutorials, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @GetMapping("/download")
+  public ResponseEntity<Resource> getFile() {
+    String filename = "tutorials.xlsx";
+    InputStreamResource file = new InputStreamResource(fileService.load());
+
+    return ResponseEntity.ok()
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+            .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+            .body(file);
   }
 }
